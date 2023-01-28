af78e52943640e9b568cd1efa34f95bfd91a452b
Zaku1234 <123833094+Zaku1234@users.noreply.github.com>
2023-01-28 21:58:35 +0100

    Update SimpleCalc.py

--- SimpleCalc.py
+++ SimpleCalc.py
@@ -1,3 +1,4 @@
+
 def calculator():
     print("Wybierz dzaialanie.")
     print("1. Podstawowe operacje")
@@ -9,69 +10,69 @@ def calculator():
     if choice == 1:
         print("1. Dodawanie")
         print("2. Odejmowanie")
-        print("3. Mnożenie")
+        print("3. Mnozenie")
         print("4. Dzielenie")
         choice = int(input("Wybierz opcje (1/2/3/4): "))
         num1 = float(input("Pierwsza liczba: "))
         num2 = float(input("Druga liczba: "))
         if choice == 1:
-            print("Wynik działania", num1, "+", num2, "=" , num1 + num2)
+            print("Wynik dzialania", num1, "+", num2, "=" , num1 + num2)
         elif choice == 2:
-            print("Wynik działania", num1, "-", num2, "=" , num1 - num2)
+            print("Wynik dzialania", num1, "-", num2, "=" , num1 - num2)
         elif choice == 3:
-            print("Wynik działania", num1, "*", num2, "=" , num1 * num2)
+            print("Wynik dzialania", num1, "*", num2, "=" , num1 * num2)
         elif choice == 4:
-            print("Wynik działania", num1, "/", num2, "=" , num1 / num2)
+            print("Wynik dzialania", num1, "/", num2, "=" , num1 / num2)
         else:
-            print("Niepoprawny wybór")
+            print("Niepoprawny wybor")
     elif choice == 2:
         print("1. Dodawanie")
         print("2. Odejmowanie")
-        print("3. Mnożenie")
+        print("3. Mnozenie")
         print("4. Transponowanie")
         choice = int(input("Wybierz opcje (1/2/3): "))
         if choice == 1:
-            matrix1 = [[float(input("Podaj wartosc dla macierzy 1: ")) for i in range(3)] for j in range(3)]
-            matrix2 = [[float(input("Podaj wartosc dla macierzy 2: ")) for i in range(3)] for j in range(3)]
+            macierz1 = [[float(input("Podaj wartosc dla macierzy 1: ")) for i in range(3)] for j in range(3)]
+            macierz2 = [[float(input("Podaj wartosc dla macierzy 2: ")) for i in range(3)] for j in range(3)]
             result = [[0 for i in range(3)] for j in range(3)]
             for i in range(3):
                 for j in range(3):
-                    result[i][j] = matrix1[i][j] + matrix2[i][j]
-            print("Macierz 1:",matrix1)
-            print("Macierz 2:",matrix2)
+                    result[i][j] = macierz1[i][j] + macierz2[i][j]
+            print("Macierz 1:",macierz1)
+            print("Macierz 2:",macierz2)
             print("Wynik: ",result)
         elif choice == 2:
-            matrix1 = [[float(input("Podaj wartosc dla macierzy 1: ")) for i in range(3)] for j in range(3)]
-            matrix2 = [[float(input("Podaj wartosc dla macierzy 2: ")) for i in range(3)] for j in range(3)]
+            macierz1 = [[float(input("Podaj wartosc dla macierzy 1: ")) for i in range(3)] for j in range(3)]
+            macierz2 = [[float(input("Podaj wartosc dla macierzy 2: ")) for i in range(3)] for j in range(3)]
             result = [[0 for i in range(3)] for j in range(3)]
             for i in range(3):
                 for j in range(3):
-                    result[i][j] = matrix1[i][j] - matrix2[i][j]
-            print("Macierz 1:",matrix1)
-            print("Macierz 2:",matrix2)
+                    result[i][j] = macierz1[i][j] - macierz2[i][j]
+            print("Macierz 1:",macierz1)
+            print("Macierz 2:",macierz2)
             print("Wynik: ",result)
         elif choice == 3:
-            matrix1 = [[float(input("Podaj wartosc dla macierzy 1: ")) for i in range(3)] for j in range(3)]
-            matrix2 = [[float(input("Podaj wartosc dla macierzy 2: ")) for i in range(3)] for j in range(3)]
+            macierz1 = [[float(input("Podaj wartosc dla macierzy 1: ")) for i in range(3)] for j in range(3)]
+            macierz2 = [[float(input("Podaj wartosc dla macierzy 2: ")) for i in range(3)] for j in range(3)]
             result = [[0 for i in range(3)] for j in range(3)]
             for i in range(3):
                 for j in range(3):
                     for k in range(3):
-                        result[i][j] += matrix1[i][k] * matrix2[k][j]
-            print("Macierz 1:",matrix1)
-            print("Macierz 2:",matrix2)
+                        result[i][j] += macierz1[i][k] * macierz2[k][j]
+            print("Macierz 1:",macierz1)
+            print("Macierz 2:",macierz2)
             print("Wynik: ",result)
         elif choice == 4:
-            matrix1 = [[float(input("Podaj wartosc dla macierzy transponowanej: ")) for i in range(3)] for j in range(3)]
+            macierz1 = [[float(input("Podaj wartosc dla macierzy transponowanej: ")) for i in range(3)] for j in range(3)]
             result = [[0 for i in range(3)] for j in range(3)]
             for i in range(3):
                 for j in range(3):
-                        result[i][j] = matrix1[j][i]
+                        result[i][j] = macierz1[j][i]
             print("Wynik: ",result)
         else:
             print("Niepoprawny wybor")
     elif choice == 3:
-        print("1. Dziesiatkowy na Dwojkow")
+        print("1. Dziesiatkowy na Dwojkowy")
         print("2. Dziesiatkowy na Osemkowy")
         print("3. Dziesiatkowy na Szesnastkowy")
         choice = int(input("Wybierz (1/2/3): "))
@@ -89,8 +90,8 @@ def calculator():
         return
     else:
         print("Niepoprawny wybor")
-
-    input("kliknij aby kontunuowac")
+        
+    input("Nacisniecie przycisku pozwoli kontynuowac dzialanie programu")
     calculator()
 
 calculator()
